2f50542808dc6384ad02111f856392a1d9d0a9dd
YshikaAgarwal <yshikaagarwal@geminisolutions.in>
2020-07-26 23:24:41 +0530

    create test file

---
+++ src/test/java/com/example/javaproject/goals/goalControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.javaproject.goals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class goalControllerTest {
+
+    @Test
+    void getAllGoals() {
+    }
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void testGetAllGoals() {
+    }
+
+    @Test
+    void getGoalById() {
+    }
+
+    @Test
+    void createGoal() {
+    }
+
+    @Test
+    void updateGoal() {
+    }
+
+    @Test
+    void deleteGoal() {
+    }
+}
